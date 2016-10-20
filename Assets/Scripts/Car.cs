6ab2f3b36df3554b1409405a12565802a4e520ed
Dmitry Dubrovin <dimandubrovin@gmail.com>
2016-10-20 21:10:33 +0700

    Episode 1

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    [Header("CAR SPECS")]
+    public float wheelRadius;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+}
---
+++ Assets/Scripts/Suspension.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Suspension : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    [Header("SUSPENSION")]
+    public float springForce;
+    public float damperForce;
+    public float springConstant;
+    public float damperConstant;
+    public float restLenght;
+
+    private float previousLenght;
+    private float currentLenght;
+    private float springVelocity;
+    private Car car;
+
+    void Start () {
+        car = transform.parent.GetComponent<Car>();
+	}
+	
+	void FixedUpdate () {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, restLenght + car.wheelRadius))
+        {
+            previousLenght = currentLenght;
+            currentLenght = restLenght - (hit.distance - car.wheelRadius);
+            springVelocity = (currentLenght - previousLenght) / Time.fixedDeltaTime;
+            springForce = springConstant * currentLenght;
+            damperForce = damperConstant * springVelocity;
+
+            rb.AddForceAtPosition(transform.up * (springForce + damperForce), transform.position);
+        }
+	}
+}
